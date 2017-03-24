61238f15d482bc403dde073704975e7eace294d1
raghsgit <raghvendrasharma41097@gmail.com>
2017-03-25 03:09:40 +0530

    deleted all

--- README.md
+++
@@ -1,5 +0,0 @@
-# form
-
-A well validated form for registration.
-gud use of javascript and html 5 validation.
-Ajax also has to be implemented.
--- js/script.js
+++
@@ -1,9 +0,0 @@
-var x = document.getElementById('degree');
-var y = document.getElementById('branch');
-
-var z = document.getElementById("name")
-var str = z.innerHTML;
-function toTitleCase(str)
-{
-    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}
